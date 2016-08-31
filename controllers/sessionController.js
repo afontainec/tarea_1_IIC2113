635d94cab748eaf38fc5ddf81550692d43f8f187
Antonio Fontaine <afontaine@uc.cl>
2016-08-31 11:10:40 -0300

    responds to different commands, it only prints stuff in console

---
+++ controllers/sessionController.js
@@ -0,0 +1,5 @@
+//controllers/sessionController.js
+
+exports.login = function(env, options) {
+    console.log("Trying to do login to %s with credentials: user: %s, password: user: %s", env, options.username, options.password);
+};
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 #!/usr/bin/env node
+
 var program = require('commander');
 
- program
-  .arguments('<file>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .action(function(file) {
-    console.log('user: %s pass: %s file: %s',
-        program.username, program.password, file);
-  })
-  .parse(process.argv);
+var sessionController = require('./controllers/sessionController.js');
+
+program
+    .version('0.0.1')
+    .option('-C, --chdir <path>', 'change the working directory')
+    .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+    .option('-T, --no-tests', 'ignore test hook');
+
+program
+    .command('login [env]')
+    .description('generate login')
+    .option("-u, --username [username]", "username for the login")
+    .option("-p, --password [password]", "password for the login")
+    .action(function(env, options) {
+        sessionController.login(env, options);
+    });
+
+program
+    .command('logout [env] ')
+    // .alias('logout [env]')
+    .description('logout from github or bitbucket ')
+    .action(function(env) {
+        console.log('logging out from %s', env);
+    }).on('--help', function() {
+        console.log('  Examples:');
+        console.log();
+        console.log('    $ saffie login github -u user -p pass');
+        console.log('    $ saffie logout github');
+        console.log();
+    });
+
+program
+    .command('*')
+    .action(function(env) {
+        console.log('Command does not exists "%s"', env);
+    });
+
+program.parse(process.argv);
